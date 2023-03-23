dd48c9e254007be4f7d6a373781d435050a7267e
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2023-03-24 01:53:22 +0530

    mohanverse

--- folder 3/app.js
+++ folder 3/app.js
@@ -9,3 +9,39 @@ document.querySelector('#football').addEventListener('click', function(e){
         target.style.backgroundColor = 'black'
     }
 })
+document.querySelector('#basketball').addEventListener('click', function(e){
+    console.log('basketball is clicked');
+
+    const target = e.target;
+
+    if(target.matches('li')){
+        target.style.backgroundColor = 'black'
+    }
+})
+document.querySelector('#boxing').addEventListener('click', function(e){
+    console.log('boxing is clicked');
+
+    const target = e.target;
+
+    if(target.matches('li')){
+        target.style.backgroundColor = 'black'
+    }
+})
+document.querySelector('#tennis').addEventListener('click', function(e){
+    console.log('tennis is clicked');
+
+    const target = e.target;
+
+    if(target.matches('li')){
+        target.style.backgroundColor = 'black'
+    }
+})
+document.querySelector('#golf').addEventListener('click', function(e){
+    console.log('golf is clicked');
+
+    const target = e.target;
+
+    if(target.matches('li')){
+        target.style.backgroundColor = 'black'
+    }
+})
